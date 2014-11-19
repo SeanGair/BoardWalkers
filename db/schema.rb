12aac353ba6a4f4f6a8341a2f0dc9ed445b73ef3
Sean Gair <gair.sean@gmail.com>
2014-11-19 16:38:56 -0600

    Added about_us database table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118180956) do
+ActiveRecord::Schema.define(version: 20141119223550) do
+
+  create_table "about_us", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
