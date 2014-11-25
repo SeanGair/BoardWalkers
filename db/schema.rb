0afea35bce0f9d45807f3096990a447f9b06df11
Sean Gair <gair.sean@gmail.com>
2014-11-25 11:53:00 -0600

    added images to products, and condition code for image and stock quantity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124163457) do
+ActiveRecord::Schema.define(version: 20141125161149) do
 
   create_table "about_us", force: true do |t|
     t.string   "title"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20141124163457) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.string   "image"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
